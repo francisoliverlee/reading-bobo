518f28b2493c57f566b444f08f32a878c0768690
tigerweili <tigerweili@tencent.com>
2023-11-27 00:25:44 +0800

    init

---
+++ script/content_script.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function(){
+    var lines = $("#read-only-cursor-text-area").text();
+    var arry = lines.split("\n");
+    for(var i = 0; i<=arry.length-1; i++){
+        console.log(arry[i])
+    }
+});
