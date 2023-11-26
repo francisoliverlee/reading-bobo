6c7f39479b52f3e49429c918b437f33cc46f9c9d
tigerweili <tigerweili@tencent.com>
2023-11-27 00:36:29 +0800

    init

--- script/content_script.js
+++ script/content_script.js
@@ -1,5 +1,13 @@
 ﻿$(document).ready(function(){
+    var file_path=$('#StickyHeader').text()
+    if (!file_path) {
+        return
+    }
     var lines = $("#read-only-cursor-text-area").text();
+    if (!lines) {
+        return
+    }
+    
     var arry = lines.split("\n");
     for(var i = 0; i<=arry.length-1; i++){
         console.log(arry[i])
